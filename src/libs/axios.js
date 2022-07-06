963bf749b0039fd39de54b5efe2dcb70e3feeb9f
ptats <tacjka34@gmail.com>
2022-07-06 16:33:07 +0200

    Add axios lib

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import config from '../config';
+import { SERVER_URL } from '../config';
 
-const instance = axios.create({ baseURL: config.SERVER_URL });
+const instance = axios.create({ baseURL: SERVER_URL });
 export default instance
