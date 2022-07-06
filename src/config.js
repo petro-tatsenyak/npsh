2d502ec5c8a5579e4c4df40fba04f16bbf9e7049
ptats <tacjka34@gmail.com>
2022-07-06 16:55:56 +0200

    Add localhost

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  SERVER_URL: process.env.REACT_APP_SERVER_URL || 'http://localhost:5000/api',
+  SERVER_URL: process.env.REACT_APP_SERVER_URL || 'http://localhost:5000',
 };
 
 export default config;
